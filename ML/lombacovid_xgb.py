74673837f92724bdd2ea6430c4318c820e3e5229
virtualmartire <boomscippo@gmail.com>
2022-07-08 23:03:28 +0200

    ordine e progressi

--- ML/lombacovid_xgb.py
+++ ML/lombacovid_xgb.py
@@ -2,67 +2,70 @@ import pandas as pd
 import numpy as np
 from math import *
 import json
-from datetime import timedelta
 import matplotlib.pyplot as plt
-
-
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 from sklearn.metrics import median_absolute_error, mean_absolute_percentage_error
-
 from sklearn.model_selection import train_test_split
-
 from xgboost import XGBRegressor
-
 from pandas.plotting import register_matplotlib_converters
+
 import seaborn as sns
 register_matplotlib_converters()
 sns.set_style("whitegrid")
 plt.rc("figure", figsize=(17, 10),dpi=100)
 plt.rc("font", size=14)
 
-##import warnings
-##warnings.filterwarnings("ignore")
+#
+##
+### FUNZIONI VARIE
+##
+#
 
+def timeserieFeatureExtractor(timeseries):
 
-
-# funzioni varie
-# ts feature engineering
-def ts_feature(timeseries):
-    timeseries['dayofweek'] = timeseries.index.dayofweek
-    timeseries['quarter'] = timeseries.index.quarter
+    #timeseries['dayofweek'] = timeseries.index.dayofweek
+    #timeseries['quarter'] = timeseries.index.quarter
     timeseries['month'] = timeseries.index.month
     timeseries['year'] = timeseries.index.year
-    timeseries['dayofyear'] = timeseries.index.dayofyear
-    timeseries['dayofmonth'] = timeseries.index.day
-    timeseries['weekofyear'] = timeseries.index.weekofyear
-    timeseries['daysinmonth'] = timeseries.index.daysinmonth
-    timeseries['weekend'] = np.where(timeseries['dayofweek'] > 4, 1, 0)
+    #timeseries['dayofyear'] = timeseries.index.dayofyear
+    #timeseries['dayofmonth'] = timeseries.index.day
+    #timeseries['weekofyear'] = timeseries.index.weekofyear
+    #timeseries['daysinmonth'] = timeseries.index.daysinmonth
+    timeseries['weekend'] = np.where(timeseries.index.dayofweek > 4, 1, 0)
     timeseries.fillna(0,inplace=True)
+
     return 
 
-# errori 
-def calc_error(y_test,y_pred):
-    R2 = r2_score(y_test,y_pred)
-    MSE = mean_squared_error(y_test,y_pred)
+def calcError(y_test, y_pred):
+
+    R2 = r2_score(y_test, y_pred)
+    MSE = mean_squared_error(y_test, y_pred)
     RMSE = sqrt(MSE)
-    MAE = mean_absolute_error(y_test,y_pred)
-    MAPE = mean_absolute_percentage_error(y_test,y_pred)
-    #maxE = max_error(y_pred,y_test)
-    medianAbsEr = median_absolute_error(y_test,y_pred)
+    MAE = mean_absolute_error(y_test, y_pred)
+    MAPE = mean_absolute_percentage_error(y_test, y_pred)
+    medianAbsEr = median_absolute_error(y_test, y_pred)
     
-    print(f'R2: {R2}')
-    print('Mean Sqarred Error: {}'.format(MSE))
-    print('Root Mean Sqarred Error: {}'.format(RMSE))
-    print('Mean Absolute Error: {}'.format(MAE))
-    print('Mean Absolute Percentage Error: {}'.format(MAPE))
-    #print('MaxError: {}'.format(maxE))
-    print('Median Absolute Error: {}'.format(medianAbsEr))
+    #print(f'R2: {R2}')
+    #print(f'Mean Squared Error: {MSE}')
+    #print(f'Root Mean Sqarred Error: {RMSE}')
+    print(f'Mean Absolute Error: {MAE}')
+    print(f'Mean Absolute Percentage Error: {MAPE}')
+    #print(f'Median Absolute Error: {medianAbsEr}')
+
     return
 
+#
+##
+### DATASET
+##
+#
 
 # lettura json
+
 f = open('ML/lombacovid_data.json.json')
 data_dict = json.load(f)
+
+# riempimento colonne
 
 max_len = len(data_dict['perc_story'])
 min_len = len(data_dict['primadose_story'])
@@ -72,84 +75,93 @@ data_dict['primadose_story'] = zeros_tofill + data_dict['primadose_story']
 data_dict['secondadose_story'] = zeros_tofill + data_dict['secondadose_story']
 data_dict['terzadose_story'] = zeros_tofill + data_dict['terzadose_story']
 
-data_covid = pd.DataFrame.from_dict(data_dict)
+# creazione dataframe
 
-data_covid.drop(columns='data', inplace=True)
-data_covid.drop(columns='terapie_story', inplace=True)
-data_covid.drop(columns='deceduti_story', inplace=True)
+dataframe = pd.DataFrame.from_dict(data_dict)
 
-data_covid['date'] = pd.date_range('2020-09-01', periods=max_len, freq='D')
-data_covid['date'] = pd.to_datetime(data_covid['date'])
-data_covid.set_index('date',inplace=True)
+dataframe.drop(columns='data', inplace=True)
+dataframe.drop(columns='terapie_story', inplace=True)
+dataframe.drop(columns='deceduti_story', inplace=True)
 
-print(data_covid.head(14))
+dataframe.rename(columns = {'ospedalizzati_story':'ospedalizzati_oggi'}, inplace = True)
 
-data_covid['ospedalizzati_story'] = data_covid['ospedalizzati_story'].shift(-7).dropna()
+dataframe['date'] = pd.date_range('2020-09-01', periods=max_len, freq='D')
+dataframe['date'] = pd.to_datetime(dataframe['date'])
+dataframe.set_index('date',inplace=True)
 
-print(data_covid.head(14))
+print(dataframe.head(10))
 
-# predizioni prossimi giorni
-# df_future_prediction = pd.DataFrame()
-# df_future_prediction['rapporto_positivi_tamponi'] = data_covid.rapporto_positivi_tamponi.tail(len(data_covid)-len(new_df))
-# df_future_prediction['prima_dose'] = data_covid.prima_dose.tail(len(data_covid)-len(new_df))
-# df_future_prediction['seconda_dose'] = data_covid.seconda_dose.tail(len(data_covid)-len(new_df))
-# df_future_prediction['terza_dose'] = data_covid.terza_dose.tail(len(data_covid)-len(new_df))
+# feature engineering
 
-# feature engineering timestamp index: uso la funzione creata
-ts_feature(data_covid)
-# ts_feature(df_future_prediction)
+past_days = 7           # <---
+future_target = 7       # <---
 
-print(data_covid.head(14))
+for i in range(1, past_days):
+    dataframe[f'ospedalizzati_past{i}'] = dataframe['ospedalizzati_oggi'].shift(i).dropna()
 
-# predizione prossimi 7 giorni
-x = data_covid.drop(columns='ospedalizzati_story')
-y = data_covid['ospedalizzati_story']
+dataframe['ospedalizzati_target'] = dataframe['ospedalizzati_oggi'].shift(-future_target).dropna()
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.85, test_size=0.15,random_state=10)
+timeserieFeatureExtractor(dataframe)
 
+print(dataframe.head(10))
 
-# creazione modello su tutto dataset (x & y)
-xgb_reg_final = XGBRegressor(
+#
+##
+### FITTING
+##
+#
+
+x = dataframe.drop(columns='ospedalizzati_target')
+y = dataframe['ospedalizzati_target']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.85, test_size=0.15, random_state=10)
+
+model = XGBRegressor(
         n_estimators=200, 
         max_depth=6, 
         learning_rate=0.1,
         random_state = 42,
-        n_jobs = 3).fit(x_train,y_train)
+        n_jobs = 3).fit(x_train, y_train)
 
+#
+##
+### TESTING
+##
+#
 
-# test dataframe
-y_pred = xgb_reg_final.predict(x_test)
+y_pred = model.predict(x_test)
 
-calc_error(y_test,y_pred)
+# calcolo degli errori
 
+print("Error on test data:")
+calcError(y_test, y_pred)
 
+# test grafico
+
+test_interval = dataframe.tail(30)
+
+fixed_interval = test_interval.head(16)['ospedalizzati_oggi']
+true_final = test_interval.tail(14)['ospedalizzati_oggi']
+pred_final = model.predict(test_interval.head(16).drop(columns='ospedalizzati_target').tail(14))
+
+pred_final = pd.DataFrame(pred_final, index=true_final.index)
+
+plt.plot(fixed_interval)
+plt.plot(true_final, label='ground truth')
+plt.plot(pred_final, label = 'prediction')
+plt.legend()
+plt.show()
 
 # # plot y_test
 # plt.rc("figure", figsize=(16, 10),dpi=100)
-
-# plt.plot(y_test.reset_index()['ospedalzizati_story'],'yo',label='test')
+# plt.plot(y_test.reset_index()['ospedalizzati_target'], 'yo', label='test')
 # plt.plot()
-# plt.plot(y_pred,'r+',markersize=12,label='pred')
+# plt.plot(y_pred, 'r+', markersize=12, label='pred')
 # plt.legend()
 # plt.title('Test set')
 # plt.show()
 
-
-
-mean_train = np.mean(y_train)   # calcolo media train 
-baseline_predictions = np.ones(y_test.shape) * mean_train  # creo vettore lungo y_test con elementi = media train
-mse_baseline = sqrt(mean_squared_error(y_test,baseline_predictions)) # calcolo errore su vettore media e y_test
-print("\nBaseline median RMSE is {:.2f}".format(mse_baseline))
-
-# # predizione di 7 giorni
-# y_next_days = xgb_reg_final.predict(df_future_prediction)
-# df_next_days = pd.DataFrame(y_next_days).set_index(df_future_prediction.index+timedelta(days=7))
-
-# # grafico predizioni della validation e validation
-# plt.plot(df_next_days,'r',markersize=9,label = 'predicted next 7 days')
-# plt.show()
-# plt.plot(data_covid.ospedalizzati,label='ospedalizzati ts')
-# plt.plot(df_next_days,'r',markersize=9,label = 'predicted next 7 days')
-# plt.legend()
-# plt.title('Ospedalizzati next 7 days')
-# plt.show()
+# mean_train = np.mean(y_train)   # calcolo media train 
+# baseline_predictions = np.ones(y_test.shape) * mean_train  # creo vettore lungo y_test con elementi = media train
+# mse_baseline = sqrt(mean_squared_error(y_test,baseline_predictions)) # calcolo errore su vettore media e y_test
+# print("\nBaseline median RMSE is {:.2f}".format(mse_baseline))
